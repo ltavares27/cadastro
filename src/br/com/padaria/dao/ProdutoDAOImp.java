7d90d984fef5e77eccc8e3f819b80fdf2dc8794f
John Doe <luispaulotav@gmail.com>
2019-09-30 13:33:05 -0400

    implementar Dao da Entidade Produto

--- src/br/com/padaria/dao/ProdutoDAOImp.java
+++ src/br/com/padaria/dao/ProdutoDAOImp.java
@@ -1,8 +1,11 @@
 package br.com.padaria.dao;
 
 import br.com.padaria.connection.ConnectionFactory;
+import br.com.padaria.domain.TipoAlimento;
+import br.com.padaria.model.Fornecedor;
 import br.com.padaria.model.Produto;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,6 +15,21 @@ import java.util.List;
 /**
  *
  * @author ltavares
+ * 
+ * 
+ * CREATE TABLE `produto` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+        `codigo` VARCHAR(255) NULL DEFAULT NULL,
+        `apelido` VARCHAR(255) NULL DEFAULT NULL,
+	`fornecedor_id` INT(11) NULL DEFAULT NULL,
+        `precoCusto` FLOAT(11) NULL DEFAULT NULL,
+        `precoFinal` FLOAT(11) NULL DEFAULT NULL,
+        `tipoAlimento` TINYINT NULL DEFAULT NULL,
+        `dataValidade` DATE NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+ * 
+ * 
  */
 public class ProdutoDAOImp implements IBaseDAO<Produto> {
     
@@ -23,15 +41,18 @@ public class ProdutoDAOImp implements IBaseDAO<Produto> {
   
     @Override
     public Produto save(Produto produto) {
-        String sql = "INSERT INTO cliente (nome, cpf, telefone, endereco, tipoCartaoFidelidade) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO produto (codigo, apelido, fornecedor_id, precoCusto, precoFinal, tipoAlimento, dataValidade)"
+                   + " VALUES (?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement stmt = null;        
         try {
             stmt = con.prepareStatement(sql);  
-//            stmt.setString(1, cliente.getNome());
-//            stmt.setString(2, cliente.getCpf());
-//            stmt.setString(3, cliente.getTelefone());
-//            stmt.setString(4, cliente.getEndereco());
-//            stmt.setObject(5, cliente.getTipoCartaoFidelidade());
+            stmt.setString(1, produto.getCodigo());
+            stmt.setString(2, produto.getApelido());
+            stmt.setObject(3, produto.getFornecedor());
+            stmt.setDouble(4, produto.getPrecoCusto());
+            stmt.setDouble(5, produto.getPrecoFinal());
+            stmt.setObject(6, produto.getTipoAlimento());           
+            stmt.setDate(7, (Date) produto.getDataValidade());           
             stmt.executeUpdate();            
         } catch (SQLException ex) {
             System.err.println("Erro ao tentar gravar dados no banco"+ ex);
@@ -44,17 +65,20 @@ public class ProdutoDAOImp implements IBaseDAO<Produto> {
     @Override
     public Produto update(Produto produto) {
         Connection con = ConnectionFactory.getConnetion();
-        String sql = "UPDATE cliente SET nome = ?, cpf = ? , telefone = ?, endereco = ?, tipoCartaoFidelidade = ?  "
+        String sql = "UPDATE produto SET codigo = ?, apelido = ?, fornecedor_id = ?, precoCusto = ?,"
+                   + "precoFinal = ?, tipoAlimento = ?, dataValidade = ?"
                    + "WHERE id = ?";
         PreparedStatement stmt = null;        
         try {
             stmt = con.prepareStatement(sql);     
-//            stmt.setString(1, cliente.getNome());
-//            stmt.setString(2, cliente.getCpf());   
-//            stmt.setString(3, cliente.getTelefone());                    
-//            stmt.setString(4, cliente.getEndereco());
-//            stmt.setObject(5, cliente.getTipoCartaoFidelidade());
-//            stmt.setInt(6, cliente.getId());
+            stmt.setString(1, produto.getCodigo());
+            stmt.setString(2, produto.getApelido());
+            stmt.setObject(3, produto.getFornecedor());
+            stmt.setDouble(4, produto.getPrecoCusto());
+            stmt.setDouble(5, produto.getPrecoFinal());
+            stmt.setObject(6, produto.getTipoAlimento());           
+            stmt.setDate(7, (Date) produto.getDataValidade());
+            stmt.setInt(8, produto.getId());
             stmt.executeUpdate();            
         } catch (SQLException ex) {
             System.err.println("Erro ao tentar gravar dados no banco "+ ex);
@@ -67,7 +91,7 @@ public class ProdutoDAOImp implements IBaseDAO<Produto> {
     @Override
     public List<Produto> findAll() {
        List<Produto> produtos =  new ArrayList<>();
-       String sql = "SELECT * FROM  cliente";
+       String sql = "SELECT * FROM  produto";
        PreparedStatement stmt = null;    
        ResultSet result = null ;
         try {
@@ -76,16 +100,17 @@ public class ProdutoDAOImp implements IBaseDAO<Produto> {
           
             while(result.next()){
               Produto produto = new Produto();
-//               cliente.setId(result.getInt("id"));
-//               cliente.setNome(result.getString("nome"));
-//               cliente.setCpf(result.getString("cpf"));
-//               cliente.setEndereco(result.getString("endereco"));
-//               cliente.setTelefone(result.getString("telefone"));
+               produto.setId(result.getInt("id"));
+               produto.setCodigo(result.getString("codigo"));
+               produto.setApelido(result.getString("appelido"));
+               produto.setPrecoCusto(result.getDouble("precoCusto"));
+               produto.setPrecoFinal(result.getDouble("precoFinal"));               
+  //             produto.setFornecedor(BuildFornecedor(result.getInt("fornecedor_id")));               
+               Integer tipoId = result.getObject("tipoAlimento", Integer.class);
+               if(tipoId != null && tipoId != 0){
+                   produto.setTipoAlimento(TipoAlimento.values()[tipoId]);          
+               }          
                
-//               Integer tipoId = result.getObject("tipoCartaoFidelidade", Integer.class);
-//               if(tipoId != null && tipoId != 0){
-//                   produto.setTipoCartaoFidelidade(TipoCartaoFidelidade.values()[tipoId]);          
-//               }          
                produtos.add(produto);
             }  
          } catch (SQLException ex){
@@ -98,11 +123,11 @@ public class ProdutoDAOImp implements IBaseDAO<Produto> {
 
     @Override
     public boolean delete(Produto produto) {
-        String sql = "DELETE FROM cliente WHERE id = ?";
+        String sql = "DELETE FROM produto WHERE id = ?";
         PreparedStatement stmt = null;        
         try {
-//            stmt = con.prepareStatement(sql);
-//            stmt.setInt(1, cliente.getId());
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1, produto.getId());
             stmt.executeUpdate();     
             return true;
         } catch (SQLException ex) {
@@ -145,4 +170,8 @@ public class ProdutoDAOImp implements IBaseDAO<Produto> {
         }
         return produto;
     }   
+
+//    private Fornecedor BuildFornecedor(Integer idFornecedor) {
+//       return new FornecedorDAOImp().findById(idFornecedor);
+//    }
 }
