1eba7da2ce99526df39931bb08ac276306556ff2
ltavares <ltavares@fazenda.ms.gov.br>
2019-10-04 14:07:16 -0400

    ajuste na tela de funcionario

--- src/br/com/padaria/view/register/TelaCadastroFuncionario.java
+++ src/br/com/padaria/view/register/TelaCadastroFuncionario.java
@@ -6,7 +6,7 @@
 package br.com.padaria.view.register;
 
 import br.com.padaria.dao.FuncionarioDAOImp;
-import br.com.padaria.model.Fornecedor;
+import br.com.padaria.domain.Cargo;
 import br.com.padaria.model.Funcionario;
 import javax.swing.JOptionPane;
 
@@ -43,13 +43,13 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jTextFieldEnderecoFuncionario = new javax.swing.JTextField();
         jLabelCpf = new javax.swing.JLabel();
-        jTextFieldCpfFuncionario = new javax.swing.JTextField();
         jLabelTelefone = new javax.swing.JLabel();
-        jTextFieldTelefoneFuncionario = new javax.swing.JTextField();
         jLabelSalario = new javax.swing.JLabel();
-        jTextFieldSalario = new javax.swing.JTextField();
         jLabelCargo = new javax.swing.JLabel();
         jComboBoxCargo = new javax.swing.JComboBox<>();
+        jFormattedTextSalario = new javax.swing.JFormattedTextField();
+        jFormattedTextCPF = new javax.swing.JFormattedTextField();
+        jFormattedTextTelefone = new javax.swing.JFormattedTextField();
         jButtonFechar = new javax.swing.JButton();
         jButtonSalvarFuncionario = new javax.swing.JButton();
 
@@ -76,21 +76,36 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
         jLabelCpf.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabelCpf.setText("CPF");
 
-        jTextFieldCpfFuncionario.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-
         jLabelTelefone.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabelTelefone.setText("Telefone");
 
         jLabelSalario.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabelSalario.setText("Salário Mensal");
 
-        jTextFieldSalario.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-
         jLabelCargo.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabelCargo.setText("Cargo");
 
         jComboBoxCargo.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jComboBoxCargo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione um Cargo", "Gerente", "Padeito", "Vendedor" }));
+
+        jFormattedTextSalario.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter()));
+        jFormattedTextSalario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jFormattedTextSalarioActionPerformed(evt);
+            }
+        });
+
+        try {
+            jFormattedTextCPF.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("###.###.###-##")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            jFormattedTextTelefone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("#####-####")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
 
         javax.swing.GroupLayout CPFLayout = new javax.swing.GroupLayout(CPF);
         CPF.setLayout(CPFLayout);
@@ -105,17 +120,21 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
                         .addComponent(jLabel2)
                         .addComponent(jTextFieldEnderecoFuncionario)
                         .addComponent(jLabelCpf)
-                        .addComponent(jTextFieldCpfFuncionario)
                         .addComponent(jLabelTelefone))
                     .addGroup(CPFLayout.createSequentialGroup()
-                        .addGroup(CPFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jTextFieldTelefoneFuncionario, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 128, Short.MAX_VALUE)
-                            .addComponent(jLabelSalario, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextFieldSalario, javax.swing.GroupLayout.Alignment.LEADING))
-                        .addGap(44, 44, 44)
+                        .addGroup(CPFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(CPFLayout.createSequentialGroup()
+                                .addComponent(jLabelSalario)
+                                .addGap(85, 85, 85))
+                            .addGroup(CPFLayout.createSequentialGroup()
+                                .addComponent(jFormattedTextSalario)
+                                .addGap(54, 54, 54)))
                         .addGroup(CPFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabelCargo)
-                            .addComponent(jComboBoxCargo, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jComboBoxCargo, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(CPFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jFormattedTextTelefone, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jFormattedTextCPF, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)))
                 .addContainerGap(16, Short.MAX_VALUE))
         );
         CPFLayout.setVerticalGroup(
@@ -131,21 +150,21 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
                 .addComponent(jTextFieldEnderecoFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabelCpf)
+                .addGap(12, 12, 12)
+                .addComponent(jFormattedTextCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldCpfFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabelTelefone)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldTelefoneFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jFormattedTextTelefone, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(CPFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabelSalario)
                     .addComponent(jLabelCargo))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(CPFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextFieldSalario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBoxCargo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(205, 205, 205))
+                    .addComponent(jComboBoxCargo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jFormattedTextSalario, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(231, 231, 231))
         );
 
         jButtonFechar.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
@@ -212,6 +231,10 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
         }        
     }//GEN-LAST:event_jButtonSalvarFuncionarioActionPerformed
 
+    private void jFormattedTextSalarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFormattedTextSalarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jFormattedTextSalarioActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -252,6 +275,9 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
     private javax.swing.JButton jButtonFechar;
     private javax.swing.JButton jButtonSalvarFuncionario;
     private javax.swing.JComboBox<String> jComboBoxCargo;
+    private javax.swing.JFormattedTextField jFormattedTextCPF;
+    private javax.swing.JFormattedTextField jFormattedTextSalario;
+    private javax.swing.JFormattedTextField jFormattedTextTelefone;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabelCargo;
@@ -260,25 +286,21 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
     private javax.swing.JLabel jLabelSalario;
     private javax.swing.JLabel jLabelTelefone;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextFieldCpfFuncionario;
     private javax.swing.JTextField jTextFieldEnderecoFuncionario;
-    private javax.swing.JTextField jTextFieldSalario;
-    private javax.swing.JTextField jTextFieldTelefoneFuncionario;
     private javax.swing.JTextPane jTextNomeFuncionario;
     // End of variables declaration//GEN-END:variables
 
     private Funcionario preencheFormulario() {
        Funcionario funcionario = new Funcionario();
        funcionario.setNome(jTextNomeFuncionario.getText());
-       funcionario.setCpf(jTextFieldCpfFuncionario.getText());
+       funcionario.setCpf(jFormattedTextCPF.getText());
        funcionario.setEndereco(jTextFieldEnderecoFuncionario.getText());
-       funcionario.setTelefone(jTextFieldTelefoneFuncionario.getText());
+       funcionario.setTelefone(jFormattedTextTelefone.getText());
+       funcionario.setSalario(Double.valueOf(jFormattedTextSalario.getText()));       
+       funcionario.setCargo(Cargo.valueOf(jComboBoxCargo.getSelectedItem().toString()));
        
-       jComboBoxCargo.getSelectedItem();
        
-       
- //      funcionario.setCargo(jListCargo);
-       funcionario.setSalario(Double.valueOf(jTextFieldSalario.getText()));
+
        
        return funcionario;
        
@@ -289,7 +311,7 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
            JOptionPane.showMessageDialog(null,"Informe o nome do Funcionario.");
            return true;
        }
-       if(jTextFieldCpfFuncionario.getText().isEmpty()) {
+       if(jTextNomeFuncionario.getText().isEmpty()) {
           JOptionPane.showMessageDialog(null,"Informe o CPF do Funcionario. ");
           return true;
        } 
@@ -297,11 +319,11 @@ public class TelaCadastroFuncionario extends javax.swing.JFrame {
           JOptionPane.showMessageDialog(null,"Informe o endereço do funcionario.");
           return true;
        }        
-       if(jTextFieldTelefoneFuncionario.getText().isEmpty()) {
+       if(jFormattedTextTelefone.getText().isEmpty()) {
           JOptionPane.showMessageDialog(null,"Informe o telefone do funcionario.");
           return true;
        }        
-       if(jTextFieldSalario.getText().isEmpty()) {
+       if(jFormattedTextSalario.getText().isEmpty()) {
           JOptionPane.showMessageDialog(null,"Informe o salário do funcionario.");
           return true;
        }        
